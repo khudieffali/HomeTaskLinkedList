343c236d59b29099ddf57fae54938cd594e722cd
Ali Xudiyev <ali.xudiyev98@gmail.com>
2024-04-28 01:43:21 +0400

    Home Task LinkedList added

---
+++ HomeTaskLinkedList/MyStack.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeTaskLinkedList
+{
+    internal class MyStack<T> : IEnumerable
+    {
+        private T[] _arr;
+        public int Count { get; set; }
+        public int Capacity { get; set; }
+
+        public MyStack()
+        {
+            Capacity = 4;
+            _arr = new T[Capacity];
+        }
+        public MyStack(int capacity)
+        {
+            Capacity = capacity;
+            _arr = new T[Capacity];
+        }
+        public MyStack(ICollection<T> item)
+        {
+            Capacity = item.Count;
+            _arr = new T[Capacity];
+            foreach (T value in item)
+            {
+                Push(value);
+            }
+        }
+
+        public void Push(T item)
+        {
+            if (Capacity == Count)
+            {
+                Capacity *= 2;
+                Array.Resize(ref _arr, Capacity);
+            }
+            _arr[Count]= item;
+            Count++;
+        }
+
+        public T Pop()
+        {
+            if (Count==0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+            T popItem = _arr[Count-1];
+            Count--;
+            return popItem;
+        }
+      
+        public T Peek()
+        {
+            if (Count == 0)
+            {
+                throw new InvalidOperationException("Stack is Empty");
+            }
+            return _arr[Count-1];
+        }
+        public IEnumerator GetEnumerator()
+        {
+            for (int i = Count-1; i >=0; i--)
+            {
+                yield return _arr[i];
+            }
+        }
+    }
+}
---
+++ HomeTaskLinkedList/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using HomeTaskLinkedList;
+
+int[] arr = { 5, 8, 9, 10, 11, 12 };
+MyStack<int> st = new MyStack<int>(arr);
+
+st.Pop();
+Console.WriteLine("Stack Pop element : ");
+Console.WriteLine(st.Pop());
+
+
+Console.WriteLine("----------------------------------");
+Console.WriteLine("Stack Peek element");
+Console.WriteLine(st.Peek());
+
+
+Console.WriteLine("----------------------------------");
+Console.WriteLine("Stack's elements :");
+foreach (var item in st)
+{
+    Console.WriteLine(item);
+
+}
+
+
+Console.WriteLine("----------------------------------");
+Console.WriteLine($"Capacity: {st.Capacity}");
+
+Console.WriteLine("----------------------------------");
+Console.WriteLine($"Count: {st.Count}");
+
+
